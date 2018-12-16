17cdc92f647d32f5f7cc692aae6ee7240e2d99ac
Jordan_M_Wofford <jmwofford@gmail.com>
2018-12-16 02:42:03 -0600

    added to views- setting up auth structure- will Freestyle the scss soon

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,16 @@
+class UserController < ApplicationController
+ 
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   root 'home#index'
-  
+    get 'user/new'
+    get 'user/edit'
+    get 'user/show'
+
+  resources :user, only:[:new, :create, :destroy]
+
+
+
+
 end
